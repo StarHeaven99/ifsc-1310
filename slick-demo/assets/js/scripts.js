7bc11cef67c535f1c974cd3fd6dce357a8d54330
StarHeaven99 <jjenkins2@ualr.edu>
2025-05-05 20:30:10 +0000

    Added demo

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,22 @@
+/* Alternate Configuation Options Here /* https://accessible360.github.io/accessible-slick/#demos */
+
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      centerMode:true,
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+  
+
+    
+
+
+});
